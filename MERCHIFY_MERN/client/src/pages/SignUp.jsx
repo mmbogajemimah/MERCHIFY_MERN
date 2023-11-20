389b4f908a0d5cd7e773f3a87716c3d587b3a99d
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-11-20 23:49:21 +0300

    Creating SignUp Page

--- MERCHIFY_MERN/client/src/pages/SignUp.jsx
+++ MERCHIFY_MERN/client/src/pages/SignUp.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+    
+      <form className='flex flex-col gap-4'>
+        <input type='text' placeholder='username' className='border p-3 rounded-lg' id='username' />
+        <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email' />
+        <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password' />
+
+        <button></button>
+      </form>
+    </div>
   )
 }
