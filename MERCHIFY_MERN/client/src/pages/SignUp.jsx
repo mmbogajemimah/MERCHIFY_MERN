10d82f5353da141b432293d3b2693722516f8542
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-11-21 22:30:04 +0300

    Complete SignUp page UI

--- MERCHIFY_MERN/client/src/pages/SignUp.jsx
+++ MERCHIFY_MERN/client/src/pages/SignUp.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function SignUp() {
   return (
@@ -10,8 +11,16 @@ export default function SignUp() {
         <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email' />
         <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password' />
 
-        <button></button>
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+          Sign Up
+        </button>
       </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Have an account?</p>
+        <Link to={"/sign-in"}>
+          <span className='text-blue-700'>Sign in</span>
+        </Link>
+      </div>
     </div>
   )
 }
