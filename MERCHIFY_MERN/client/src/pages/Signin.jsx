441c88e881bc2517a42dcf7aa6a1399ef9930bfe
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-11-24 12:01:06 +0300

    Add Redux Toolkit

--- MERCHIFY_MERN/client/src/pages/Signin.jsx
+++ MERCHIFY_MERN/client/src/pages/Signin.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
 
   const handleChange = (e) => {
@@ -21,7 +24,7 @@ export default function SignIn() {
   const handleSubmit = async(e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: "POST",
         headers: {'Content-Type': 'application/json'},
@@ -29,17 +32,14 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (data.success === false) {
-        setError(data.message);
-        setLoading(false);
+        dispatch(signInFailure(data.message));
         return;
       };
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data))
       navigate('/');
       console.log(data);
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message));
     }
   };
 
