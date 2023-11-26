bd2d757cf480535aeef98575c54c8351157bb4f9
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-11-26 23:57:56 +0300

    Adding Google Oath to logging in and Signing up into Merchify

---
+++ MERCHIFY_MERN/client/src/components/OAuth.jsx
@@ -0,0 +1,35 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+
+import React from 'react'
+import { app } from '../firebase';
+
+export default function OAuth() {
+
+  const handleGoogleClick = async () => {
+    try {
+        const provider = new GoogleAuthProvider()
+        const auth = getAuth(app)
+
+        const result = await signInWithPopup(auth, provider)
+
+        const res = await fetch ('/api/auth/google', {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify({name: result.user.displayName, email: result.user.email, photo: result.user.photoURL }),
+        })
+        const data = await res.json()
+
+        // console.log("Result >>>", result)
+    } catch (error) {
+        console.log('Could not sign in with google', error);
+    }
+  };
+
+  return (
+    <button onClick={handleGoogleClick} type='button' className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>
+        Continue with Google
+    </button>
+  )
+}
