63c5be5d32504291c78e14fb58238aaf00fba738
Purity Ruth <purityruth09@gmail.com>
2023-11-28 12:29:20 +0300

    Create and Add OAth Component

--- MERCHIFY_MERN/api/models/user.model.js
+++ MERCHIFY_MERN/api/models/user.model.js
@@ -14,7 +14,11 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-    }
+    },
+    avatar: {
+        type: String,
+        default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fprofile&psig=AOvVaw0OjrrC0dv-4BZzlqSUNsj2&ust=1701248464012000&source=images&cd=vfe&ved=0CBIQjRxqFwoTCIDI06iq5oIDFQAAAAAdAAAAABAQ"
+    },
 }, { timestamps: true});
 
 const User = mongoose.model('User', userSchema);
--- MERCHIFY_MERN/client/src/components/OAuth.jsx
+++ MERCHIFY_MERN/client/src/components/OAuth.jsx
@@ -4,10 +4,12 @@ import React from 'react'
 import { app } from '../firebase';
 import { useDispatch } from 'react-redux';
 import { signInSuccess } from '../redux/user/userSlice';
+import { useNavigate } from 'react-router-dom';
 
 export default function OAuth() {
   
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
   const handleGoogleClick = async () => {
     try {
@@ -25,7 +27,7 @@ export default function OAuth() {
         })
         const data = await res.json();
         dispatch(signInSuccess(data));
-
+        navigate('/');
         // console.log("Result >>>", result)
     } catch (error) {
         console.log('Could not sign in with google', error);
