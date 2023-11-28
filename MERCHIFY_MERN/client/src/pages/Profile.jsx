b36a69292e36e18c90cc58b4ce9db6799dde3710
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-11-28 23:14:01 +0300

    Upload Profile Image to Firebase Storage

--- MERCHIFY_MERN/client/src/pages/Profile.jsx
+++ MERCHIFY_MERN/client/src/pages/Profile.jsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../firebase';
 
 export default function Profile() {
+  const fileRef = useRef(null)
   const {currentUser} = useSelector((state) => state.user);
+  const [ file, setFile ] = useState(undefined);
+  const [ filePerc, setFilePerc ] = useState(0);
+  console.log(filePerc)
+
+  console.log(file);
+
+  useEffect(() => {
+    if(file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed',
+    (snapshot) => {
+      const progress = (snapshot.bytesTransferred /
+      snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+      setFilePerc(Math.round(progress));
+    },
+    )
+  }
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -10,7 +40,8 @@ export default function Profile() {
         Profile
       </h1>
       <form className='flex flex-col gap-4'>
-        <img src={currentUser.avatar} alt='profile' 
+        <input onChange={(e) => setFile(e.target.files[0])} type='file' ref={fileRef} hidden accept='image/*'/>
+        <img onClick={() => fileRef.current.click()} src={currentUser.avatar} alt='profile' 
         className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'/>
 
         <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' />
