f4780f72b7fb73bab03d7439a29f7f6b7a6eb03d
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-11-29 23:43:58 +0300

    Complete Image Upload Functionality

--- MERCHIFY_MERN/client/src/pages/Profile.jsx
+++ MERCHIFY_MERN/client/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from '../firebase';
 
 export default function Profile() {
@@ -8,9 +8,12 @@ export default function Profile() {
   const {currentUser} = useSelector((state) => state.user);
   const [ file, setFile ] = useState(undefined);
   const [ filePerc, setFilePerc ] = useState(0);
-  console.log(filePerc)
+  const [ fileUploadError, setFileUploadError ] = useState(false);
+  const [ formData, setFormData ] = useState({});
 
-  console.log(file);
+  console.log(filePerc);
+  console.log(fileUploadError);
+  console.log(formData);
 
   useEffect(() => {
     if(file) {
@@ -31,7 +34,15 @@ export default function Profile() {
       console.log('Upload is ' + progress + '% done');
       setFilePerc(Math.round(progress));
     },
-    )
+    (error) => {
+      setFileUploadError(true);
+    },
+    () => {
+      getDownloadURL(uploadTask.snapshot.ref).then
+      ((downloadURL) => {
+        setFormData({...formData, avatar: downloadURL});
+      })}
+    );
   }
 
   return (
@@ -41,8 +52,19 @@ export default function Profile() {
       </h1>
       <form className='flex flex-col gap-4'>
         <input onChange={(e) => setFile(e.target.files[0])} type='file' ref={fileRef} hidden accept='image/*'/>
-        <img onClick={() => fileRef.current.click()} src={currentUser.avatar} alt='profile' 
+        <img onClick={() => fileRef.current.click()} src={ formData.avatar || currentUser.avatar} alt='profile' 
         className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'/>
+
+        <p className='text-sm self-center'>
+          {fileUploadError ? (
+            <span className='text-red-700'>Error Image upload(Image must be less than 2mb)</span>
+          ) : filePerc > 0 && filePerc < 100 ? (
+            <span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>
+          ) : filePerc === 100 ? (
+            <span className='text-green-700'>Image successfully Uploaded!</span>
+          ) : ('')
+          }
+        </p>
 
         <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' />
         <input type="text" placeholder='email' className='border p-3 rounded-lg' id='email'/>
