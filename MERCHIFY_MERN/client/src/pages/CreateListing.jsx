3e89f2e9453283a8a734004200b3d84f9b9ae961
Purity Ruth <purityruth09@gmail.com>
2023-12-18 16:48:07 +0300

    Create Listing

--- MERCHIFY_MERN/client/src/pages/CreateListing.jsx
+++ MERCHIFY_MERN/client/src/pages/CreateListing.jsx
@@ -1,6 +1,17 @@
 // import React from 'react'
 
+import { useState } from "react"
+
 export default function CreateListing() {
+  const [files, setFiles] = useState([]);
+  console.log(files);
+
+  const handleImageSubmit = (e) => {
+    if (files.length > 0 && files.length < 7){
+      const promises = [];
+    }
+  }
+
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Create a Listing</h1>
@@ -75,10 +86,10 @@ export default function CreateListing() {
             <span className="font-normal text-gray-700 ml-2">The first image will be the cover (max 6)</span>
           </p>
           <div className="flex gap-4">
-            <input className="p-3 border border-gray-300 rounded w-full" type="file" id='images' accept='images/*' multiple />
+            <input onChange={(e) => setFiles(e.target.files)} className="p-3 border border-gray-300 rounded w-full" type="file" id='images' accept='images/*' multiple />
             <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">Upload</button>
           </div>
-          <button className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          <button type='button' onClick={handleImageSubmit} className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80 my-4">
             Create Listing
           </button>
         </div>
