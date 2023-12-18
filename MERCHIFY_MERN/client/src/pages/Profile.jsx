0da5e9dff243d1423ef3a944537e665a23c0f351
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-12-18 07:31:58 +0300

    ui

--- MERCHIFY_MERN/client/src/pages/Profile.jsx
+++ MERCHIFY_MERN/client/src/pages/Profile.jsx
@@ -5,6 +5,7 @@ import { app } from '../firebase';
 import { updateUserFailure, updateUserSuccess, updateUserStart, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart, signOutUserFailure, signOutUserSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import { current } from '@reduxjs/toolkit';
+import { Link } from 'react-router-dom';
 
 export default function Profile() {
   const fileRef = useRef(null)
@@ -139,6 +140,9 @@ export default function Profile() {
         <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95'>
           {loading ? 'Loading...' : 'Update'}
         </button>
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to={"/create-listing"}>
+          Create Listing
+        </Link>
       </form>
       <div  className='flex justify-between mt-5'>
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete account</span>
