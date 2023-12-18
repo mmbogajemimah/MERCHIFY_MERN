30279a6f6256bcf82f5f0daac70d4b9d4e5a7e96
Purity Ruth <purityruth09@gmail.com>
2023-12-18 12:27:49 +0300

    Complete Create-Listing User Interface

--- MERCHIFY_MERN/client/src/App.jsx
+++ MERCHIFY_MERN/client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Signin from './pages/Signin';
@@ -7,6 +7,7 @@ import About from './pages/About';
 import SignUp from './pages/SignUp';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
+import CreateListing from './pages/CreateListing';
 
 export default function App() {
   return (
@@ -19,6 +20,8 @@ export default function App() {
         <Route path="/about" element={<About />}/>
         <Route element={<PrivateRoute /> } >
           <Route path="/profile" element={<Profile />}/>
+          <Route path="/create-listing" element={<CreateListing />}/>
+
         </Route>
       </Routes>
     </BrowserRouter>
