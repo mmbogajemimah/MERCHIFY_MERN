a0fdbe6306fd6e9965a2809973e6962f8c97095b
Purity Ruth <purityruth09@gmail.com>
2023-12-28 12:02:04 +0300

    Complete Upload Listing Images Functionalities

--- MERCHIFY_MERN/client/src/pages/CreateListing.jsx
+++ MERCHIFY_MERN/client/src/pages/CreateListing.jsx
@@ -9,12 +9,16 @@ export default function CreateListing() {
   const [ formData, setFormData ] = useState({
     imageUrls: [],
   });
+  const [ imageUploadError, setImageUploadError ] = useState(false);
+  const [ uploading, setUploading ] = useState(false);
 
   console.log(formData)
   // console.log(files);
 
   const handleImageSubmit = (e) => {
-    if (files.length > 0 && files.length < 7){
+    if (files.length > 0 && files.length + formData.imageUrls.length < 7){
+      setUploading(true);
+      setImageUploadError(false);
       const promises = [];
 
       for (let i = 0; i < files.length; i++) {
@@ -25,7 +29,16 @@ export default function CreateListing() {
           ...formData,
           imageUrls: formData.imageUrls.concat(urls),
         });
+        setImageUploadError(false);
+        setUploading(false);
+      })
+      .catch((err) => {
+        setImageUploadError('Image upload failed (2 mb max per image)');
+        setUploading(false);
       });
+    } else {
+      setImageUploadError("You can only add up to 6 images per listing.");
+      setUploading(false);
     }
   };
 
@@ -51,6 +64,13 @@ export default function CreateListing() {
         }
       )
     });
+  }
+
+  const handleRemoveImage = (index) => {
+    setFormData({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_, i) => i !== index),
+    })
   }
 
   return (
@@ -128,9 +148,32 @@ export default function CreateListing() {
           </p>
           <div className="flex gap-4">
             <input onChange={(e) => setFiles(e.target.files)} className="p-3 border border-gray-300 rounded w-full" type="file" id='images' accept='images/*' multiple />
-            <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">Upload</button>
+            <button type='button' disabled={uploading} onClick={handleImageSubmit} className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">
+              { uploading ? 'Uploading...' : 'Upload' }
+            </button>
           </div>
-          <button type='button' onClick={handleImageSubmit} className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80 my-4">
+          <p className="text-red-700 my-2">{imageUploadError && imageUploadError}</p>
+          {formData.imageUrls.length > 0 &&
+            formData.imageUrls.map((url, index) => (
+              <div
+                key={url}
+                className='flex justify-between p-3 border items-center'
+              >
+                <img
+                  src={url}
+                  alt='listing image'
+                  className='w-20 h-20 object-contain rounded-lg'
+                />
+                <button
+                  type='button'
+                  onClick={() => handleRemoveImage(index)}
+                  className='p-3 text-red-700 rounded-lg uppercase hover:opacity-75'
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
+          <button className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80 my-4">
             Create Listing
           </button>
         </div>
