5dc11ec9f1be8de2d5315e4cad31adef144174cf
Jemimah Mmboga <mmbogajemimah@gmail.com>
2024-01-07 22:42:41 +0300

    Search Page

---
+++ MERCHIFY_MERN/client/src/pages/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Search() {
+  return (
+    <div className='flex flex-col md:flex-row'>
+        <div className='p-7 border-b-2 md:min-h-screen'>
+
+        </div>
+    </div>
+  )
+}
