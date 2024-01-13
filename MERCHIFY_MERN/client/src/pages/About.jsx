0b589f70ae3a357331a4c968cfaaaf27ad9c9481
Jemimah Mmboga <mmbogajemimah@gmail.com>
2024-01-13 10:06:59 +0300

    Complete About Page

--- MERCHIFY_MERN/client/src/pages/About.jsx
+++ MERCHIFY_MERN/client/src/pages/About.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About Page</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Mernify</h1>
+      <p className='mb-4 text-slate-700'>Merchify stands out with its sleek and responsive design, offering a visually appealing interface that caters to a diverse range of tastes and preferences. The application is equipped with robust search and filter functionalities, allowing users to easily discover their favorite products or explore new and trending items.</p>
+      <p className='mb-4 text-slate-700'>For sellers, Merchify provides a comprehensive dashboard that simplifies inventory management, order processing, and customer interactions. Integration with secure payment gateways ensures a seamless and trustworthy transaction process, fostering a sense of reliability for both buyers and sellers.</p>
+      <p className='mb-4 text-slate-700'>Merchify also takes advantage of advanced analytics to provide valuable insights for sellers, helping them understand customer behavior, optimize marketing strategies, and make informed decisions to grow their online businesses. The application is designed to be scalable, accommodating businesses of all sizes, from individual artists and creators to established brands.</p>
+      <p className='mb-4 text-slate-700'>With Merchify, the world of online merchandise becomes more accessible, engaging, and efficient for both buyers and sellers, making it a go-to platform for anyone looking to navigate the digital marketplace with ease and success.</p>
+    </div>
   )
 }
--- MERCHIFY_MERN/client/src/pages/Search.jsx
+++ MERCHIFY_MERN/client/src/pages/Search.jsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import ListingItem from '../components/ListingItem';
 
 export default function Search() {
   const navigate = useNavigate();
-  const location = useLocation()
   const [sidebardata, setSidebardata] = useState({
     searchTerm: '',
     type: 'all',
@@ -18,7 +17,6 @@ export default function Search() {
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
   const [showMore, setShowMore] = useState(false);
-  // console.log(listings)
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -43,12 +41,13 @@ export default function Search() {
         searchTerm: searchTermFromUrl || '',
         type: typeFromUrl || 'all',
         parking: parkingFromUrl === 'true' ? true : false,
-        furnished: furnishedFromUrl === 'true' ? true : false, 
+        furnished: furnishedFromUrl === 'true' ? true : false,
         offer: offerFromUrl === 'true' ? true : false,
         sort: sortFromUrl || 'created_at',
         order: orderFromUrl || 'desc',
       });
     }
+
     const fetchListings = async () => {
       setLoading(true);
       setShowMore(false);
@@ -65,9 +64,7 @@ export default function Search() {
     };
 
     fetchListings();
-  }, [location.search])
-
-  // console.log(sidebardata)
+  }, [location.search]);
 
   const handleChange = (e) => {
     if (
@@ -75,11 +72,11 @@ export default function Search() {
       e.target.id === 'rent' ||
       e.target.id === 'sale'
     ) {
-      setSidebardata({ ...sidebardata, type: e.target.id});
+      setSidebardata({ ...sidebardata, type: e.target.id });
     }
 
     if (e.target.id === 'searchTerm') {
-      setSidebardata({...sidebardata, searchTerm: e.target.value});
+      setSidebardata({ ...sidebardata, searchTerm: e.target.value });
     }
 
     if (
@@ -89,16 +86,17 @@ export default function Search() {
     ) {
       setSidebardata({
         ...sidebardata,
-        [e.target.id]: 
+        [e.target.id]:
           e.target.checked || e.target.checked === 'true' ? true : false,
       });
     }
 
     if (e.target.id === 'sort_order') {
       const sort = e.target.value.split('_')[0] || 'created_at';
+
       const order = e.target.value.split('_')[1] || 'desc';
 
-      setSidebardata({...sidebardata, sort, order});
+      setSidebardata({ ...sidebardata, sort, order });
     }
   };
 
@@ -129,117 +127,138 @@ export default function Search() {
     }
     setListings([...listings, ...data]);
   };
-
   return (
     <div className='flex flex-col md:flex-row'>
-      <div className='p-7 border-b-2 md:border-r-2 md:min-h-screen'>
+      <div className='p-7  border-b-2 md:border-r-2 md:min-h-screen'>
         <form onSubmit={handleSubmit} className='flex flex-col gap-8'>
           <div className='flex items-center gap-2'>
-            <label className='whitespace-nowrap font-semibold'>Search Term:</label>
-            <input type='text' id='searchTerm' placeholder='Search...' className='border rounded-lg p-3 w-full' 
-              value = {sidebardata.searchTerm}
+            <label className='whitespace-nowrap font-semibold'>
+              Search Term:
+            </label>
+            <input
+              type='text'
+              id='searchTerm'
+              placeholder='Search...'
+              className='border rounded-lg p-3 w-full'
+              value={sidebardata.searchTerm}
               onChange={handleChange}
             />
-            
           </div>
-
           <div className='flex gap-2 flex-wrap items-center'>
             <label className='font-semibold'>Type:</label>
             <div className='flex gap-2'>
-              <input type='checkbox' id='all' className='w-5' 
-              
+              <input
+                type='checkbox'
+                id='all'
+                className='w-5'
                 onChange={handleChange}
                 checked={sidebardata.type === 'all'}
               />
               <span>Rent & Sale</span>
             </div>
-
             <div className='flex gap-2'>
-              <input type='checkbox' id='rent' className='w-5' 
+              <input
+                type='checkbox'
+                id='rent'
+                className='w-5'
                 onChange={handleChange}
                 checked={sidebardata.type === 'rent'}
               />
               <span>Rent</span>
             </div>
-
             <div className='flex gap-2'>
-              <input type='checkbox' id='sale' className='w-5' 
+              <input
+                type='checkbox'
+                id='sale'
+                className='w-5'
                 onChange={handleChange}
                 checked={sidebardata.type === 'sale'}
               />
               <span>Sale</span>
             </div>
-
             <div className='flex gap-2'>
-              <input type='checkbox' id='offer' className='w-5' 
+              <input
+                type='checkbox'
+                id='offer'
+                className='w-5'
                 onChange={handleChange}
                 checked={sidebardata.offer}
               />
               <span>Offer</span>
             </div>
           </div>
-
           <div className='flex gap-2 flex-wrap items-center'>
             <label className='font-semibold'>Amenities:</label>
             <div className='flex gap-2'>
-              <input type='checkbox' id='parking' className='w-5' 
+              <input
+                type='checkbox'
+                id='parking'
+                className='w-5'
                 onChange={handleChange}
                 checked={sidebardata.parking}
               />
               <span>Parking</span>
             </div>
-
             <div className='flex gap-2'>
-              <input type='checkbox' id='furnished' className='w-5' 
+              <input
+                type='checkbox'
+                id='furnished'
+                className='w-5'
                 onChange={handleChange}
                 checked={sidebardata.furnished}
               />
               <span>Furnished</span>
             </div>
           </div>
-
           <div className='flex items-center gap-2'>
             <label className='font-semibold'>Sort:</label>
-            <select 
-            onChange={handleChange}
-            defaultValue={'created_at_desc'}
-            id='sort_order' className='border rounded-lg p-3'>
+            <select
+              onChange={handleChange}
+              defaultValue={'created_at_desc'}
+              id='sort_order'
+              className='border rounded-lg p-3'
+            >
               <option value='regularPrice_desc'>Price high to low</option>
-              <option value='regularPrice_asc'>Price low to high</option>
+              <option value='regularPrice_asc'>Price low to hight</option>
               <option value='createdAt_desc'>Latest</option>
               <option value='createdAt_asc'>Oldest</option>
             </select>
           </div>
-          <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>Search</button>
+          <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>
+            Search
+          </button>
         </form>
       </div>
-
       <div className='flex-1'>
-        <h1 className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>Listing Results: </h1>
+        <h1 className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>
+          Listing results:
+        </h1>
         <div className='p-7 flex flex-wrap gap-4'>
           {!loading && listings.length === 0 && (
             <p className='text-xl text-slate-700'>No listing found!</p>
           )}
           {loading && (
-            <p className='text-xl text-slate-700 text-center w-full'>Loading...</p>
+            <p className='text-xl text-slate-700 text-center w-full'>
+              Loading...
+            </p>
           )}
+
           {!loading &&
             listings &&
             listings.map((listing) => (
               <ListingItem key={listing._id} listing={listing} />
             ))}
-            {showMore && (
-              <button
-                onClick={() => {
-                  onShowMoreClick()
-                }}
-                className='text-green-700 hover:underline p-7 text-center w-full'
-              >
-                Show More
-              </button>
-            )}
+
+          {showMore && (
+            <button
+              onClick={onShowMoreClick}
+              className='text-green-700 hover:underline p-7 text-center w-full'
+            >
+              Show more
+            </button>
+          )}
         </div>
       </div>
     </div>
-  )
+  );
 }
